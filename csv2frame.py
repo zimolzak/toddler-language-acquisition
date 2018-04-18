57eb9025b65472ca17e51c2d362915ea6133064a
Andrew Zimolzak <andyzimolzak@gmail.com>
2018-04-18 09:10:11 -0400

    Turn all that into a function, thus fewer globals.

--- csv2frame.py
+++ csv2frame.py
@@ -1,13 +1,15 @@
 import pandas as pd
 INPUT_FILENAME = 'day_per_row.csv'
-LINES = open(INPUT_FILENAME, 'r').read().splitlines()
-DATE_LIST = []
-WORD_LIST = []
-for L in LINES:
-    line_list = L.split(',')
-    for i in range(1,len(line_list)): # start w/ 1 not 0.
-        DATE_LIST.append(line_list[0])
-        WORD_LIST.append(line_list[i])
 
-DF = pd.DataFrame({'date':DATE_LIST, 'word':WORD_LIST})
-print(DF)
+def filename2frame(fn):
+    lines = open(fn, 'r').read().splitlines()
+    date_list = []
+    word_list = []
+    for L in lines:
+        line_list = L.split(',')
+        for i in range(1,len(line_list)): # start w/ 1 not 0.
+            date_list.append(line_list[0])
+            word_list.append(line_list[i])
+    return pd.DataFrame({'date':date_list, 'word':word_list})
+
+print(filename2frame(INPUT_FILENAME))
