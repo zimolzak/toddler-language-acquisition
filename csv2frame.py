41317c82d167fcea28d9fa2e3835211f488b750c
Andrew Zimolzak <andyzimolzak@gmail.com>
2018-04-18 09:20:10 -0400

    Sensible documentation and variable naming.

--- csv2frame.py
+++ csv2frame.py
@@ -2,14 +2,23 @@ import pandas as pd
 INPUT_FILENAME = 'day_per_row.csv'
 
 def filename2frame(fn):
-    lines = open(fn, 'r').read().splitlines()
+    """Make a pandas DataFrame from a shorthand CSV.
+
+    In other words, unroll the CSV into a DataFrame. It's a slightly
+    unusual CSV in that column 0 is always a date, and then there are
+    a *variable* number of subsequent columns that you can think of as
+    word0, word1, ... wordn. This compact / rolled-up structure simply
+    allows me to type the CSV faster.
+    """
+    rows = open(fn, 'r').read().splitlines()
     date_list = []
     word_list = []
-    for L in lines:
-        line_list = L.split(',')
-        for i in range(1,len(line_list)): # start w/ 1 not 0.
-            date_list.append(line_list[0])
-            word_list.append(line_list[i])
+    for r in rows:
+        columns = r.split(',')
+        # Assume columns[0] is always date and columns[1..end] are words.
+        for i in range(1,len(columns)):
+            date_list.append(columns[0])
+            word_list.append(columns[i])
     return pd.DataFrame({'date':date_list, 'word':word_list})
 
 print(filename2frame(INPUT_FILENAME))
